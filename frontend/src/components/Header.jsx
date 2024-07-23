595a966175c472f48054231369f6e0833ace78c4
Mangesh <mangesh13salunke@gmail.com>
2024-07-23 14:34:11 +0530

    create signup frontend part

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
@@ -22,7 +22,7 @@ export default function Header() {
             <ul className='flex gap-4'>
                 <Link to={"/"}> <li className='hidden sm:inline text-slate-700 hover:cursor-pointer hover:font-bold'>Home</li> </Link>
                 <Link to={"/about"}> <li className='hidden sm:inline text-slate-700 hover:cursor-pointer hover:font-bold'>About</li> </Link>
-                <Link to={"/signin"}> <li className='hidden sm:inline text-slate-700 hover:cursor-pointer hover:font-bold'>SignIn</li> </Link>
+                <Link to={"/signup"}> <li className='hidden sm:inline text-slate-700 hover:cursor-pointer hover:font-bold'>SignUp</li> </Link>
 
             </ul>
         </div>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className='p-3 max-w-lg mx-auto'>
+
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+
+      <form className='flex flex-col gap-4'>
+        <input type="text" id="username" placeholder="Enter Your Username" className='border p-3 round' />
+        <input type="email" id="email" placeholder="Enter Your Email" className='border p-3 round' />
+        <input type="password" id="password" placeholder="Enter Your Password" className='border p-3 round' />
+
+        <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-85'>SUBMIT</button>
+      </form>
+
+      <div className='flex gap-3 py-3'>
+        <p className='font-semibold'>Haven an account?</p>
+        <Link to={"/signin"}>
+          <span className='text-blue-700 font-semibold'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
 }
