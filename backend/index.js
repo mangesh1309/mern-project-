c3b3de7370b0de16a3e3e924a60ad18ac9c8f51a
Mangesh <mangesh13salunke@gmail.com>
2024-07-23 13:28:23 +0530

    create test route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import userRouter from "./routes/user-route.js";
 
 mongoose.connect(process.env.MONGO_DB)
 .then(() => {
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO_DB)
 });
 
 const app = express();
+
+app.use("/backend/user", userRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
