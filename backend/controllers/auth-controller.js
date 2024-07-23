1ea16a9d314eed2af1ca24d1b00a436477eaa1ac
Mangesh <mangesh13salunke@gmail.com>
2024-07-23 15:31:13 +0530

    create signin route

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,7 +1,7 @@
 import User from './../models/user-model.js';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
-
+import jwt from 'jsonwebtoken'
 // Load environment variables from .env file
 dotenv.config();
 
@@ -23,3 +23,30 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, 'User not found!'));
+  
+      const validPassword = await bcrypt.compare(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, 'Invalid credentials!'));
+  
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      const { password: pass, ...rest } = validUser._doc;
+  
+      res
+        .cookie('access_token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production', 
+          sameSite: 'strict', 
+        })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
